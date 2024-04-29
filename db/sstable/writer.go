48d264d05fc4bbdb8649cf1f3541630e44b6907f
gptjddldi <hystom38@gmail.com>
2024-04-29 13:52:33 +0900

    feat: Add sst index block

--- db/sstable/writer.go
+++ db/sstable/writer.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"io"
+	"log"
 	"lsm/db/memtable"
 )
 
@@ -14,9 +15,11 @@ type syncCloser interface {
 }
 
 type Writer struct {
-	file syncCloser
-	bw   *bufio.Writer
-	buf  *bytes.Buffer
+	file       syncCloser
+	bw         *bufio.Writer
+	buf        *bytes.Buffer
+	offsets    []uint32
+	nextOffset uint32
 }
 
 func NewWriter(file io.Writer) *Writer {
@@ -33,26 +36,25 @@ func (w *Writer) Process(m *memtable.Memtable) error {
 	i := m.Iterator()
 	for i.HasNext() {
 		key, val := i.Next()
-		err := w.writeDataBlock(key, val)
-
+		n, err := w.writeDataBlock(key, val)
 		if err != nil {
 			return err
 		}
+		w.addIndexEntry(n)
+	}
+	err := w.writeIndexBlock()
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
 // *.sst file format:
 // [keyLen:2][valLen:2][key:keyLen][encodedValue:valLen (OpType + value)]
-func (w *Writer) writeDataBlock(key, val []byte) error {
+func (w *Writer) writeDataBlock(key, val []byte) (int, error) {
 	keyLen, valLen := len(key), len(val)
 	needed := 2*binary.MaxVarintLen64 + keyLen + valLen
-	available := w.buf.Available()
-	if needed > available {
-		w.buf.Grow(needed)
-	}
-	buf := w.buf.AvailableBuffer()
-	buf = buf[:needed]
+	buf := w.scratchBuf(needed)
 	n := binary.PutUvarint(buf, uint64(keyLen))
 	n += binary.PutUvarint(buf[n:], uint64(valLen))
 	copy(buf[n:], key)
@@ -60,13 +62,13 @@ func (w *Writer) writeDataBlock(key, val []byte) error {
 	used := n + keyLen + valLen
 	_, err := w.buf.Write(buf[:used])
 	if err != nil {
-		return err
+		return n, err
 	}
-	_, err = w.bw.ReadFrom(w.buf)
+	m, err := w.bw.ReadFrom(w.buf)
 	if err != nil {
-		return err
+		return int(m), err
 	}
-	return nil
+	return int(m), nil
 }
 
 func (w *Writer) Close() error {
@@ -89,4 +91,33 @@ func (w *Writer) Close() error {
 	w.file = nil
 
 	return err
+}
+
+func (w *Writer) scratchBuf(needed int) []byte {
+	available := w.buf.Available()
+	if needed > available {
+		w.buf.Grow(needed)
+	}
+	buf := w.buf.AvailableBuffer()
+	return buf[:needed]
+}
+
+func (w *Writer) addIndexEntry(n int) {
+	w.offsets = append(w.offsets, w.nextOffset)
+	w.nextOffset += uint32(n)
+}
+
+func (w *Writer) writeIndexBlock() error {
+	numOffsets := len(w.offsets)
+	needed := (numOffsets + 1) * 4
+	buf := w.scratchBuf(needed)
+	for i, offset := range w.offsets {
+		binary.LittleEndian.PutUint32(buf[i*4:i*4+4], offset)
+	}
+	_, err := w.bw.Write(buf[:])
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
 }
