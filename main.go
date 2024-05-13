123cedc92d75aef988b6f2f4e85321e69e2a2660
gptjddldi <hystom38@gmail.com>
2024-05-13 19:16:57 +0900

    Revert "temp annotation"
    This reverts commit a30bf065bce3c411ac53495593887229acd5702f.

--- main.go
+++ main.go
@@ -1,64 +1,64 @@
 package lsm
 
-// import (
-// 	"bufio"
-// 	"flag"
-// 	"fmt"
-// 	"github.com/gptjddldi/lsm/cli"
-// 	"log"
-// 	"os"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/gptjddldi/lsm/cli"
+	"log"
+	"os"
 
-// 	"github.com/go-faker/faker/v4"
-// )
+	"github.com/go-faker/faker/v4"
+)
 
-// const dataFolder = "demo-data"
+const dataFolder = "demo-data"
 
-// var shouldReset, shouldSeed *bool
-// var seedNumRecords *int
+var shouldReset, shouldSeed *bool
+var seedNumRecords *int
 
-// func main() {
-// 	setupFlags()
+func main() {
+	setupFlags()
 
-// 	if *shouldReset {
-// 		eraseDataFolder()
-// 	}
+	if *shouldReset {
+		eraseDataFolder()
+	}
 
-// 	d, err := Open(dataFolder)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	d, err := Open(dataFolder)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	if *shouldSeed {
-// 		seedDatabaseWithTestRecords(d)
-// 	}
+	if *shouldSeed {
+		seedDatabaseWithTestRecords(d)
+	}
 
-// 	scanner := bufio.NewScanner(os.Stdin)
-// 	demo := cli.NewCLI(scanner, d)
-// 	demo.Start()
-// }
+	scanner := bufio.NewScanner(os.Stdin)
+	demo := cli.NewCLI(scanner, d)
+	demo.Start()
+}
 
-// func setupFlags() {
-// 	shouldReset = flag.Bool("reset", false, "Reset the database by erasing its folder before startup.")
-// 	shouldSeed = flag.Bool("seed", false, "Seed the database using records created with go-faker.")
-// 	seedNumRecords = flag.Int("records", 1000, "Amount of records to seed the database with upon startup.")
-// 	flag.Usage = func() {
-// 		fmt.Println("\nDB CLI\n\nArguments:")
-// 		flag.PrintDefaults()
-// 	}
-// 	flag.Parse()
-// }
+func setupFlags() {
+	shouldReset = flag.Bool("reset", false, "Reset the database by erasing its folder before startup.")
+	shouldSeed = flag.Bool("seed", false, "Seed the database using records created with go-faker.")
+	seedNumRecords = flag.Int("records", 1000, "Amount of records to seed the database with upon startup.")
+	flag.Usage = func() {
+		fmt.Println("\nDB CLI\n\nArguments:")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+}
 
-// func eraseDataFolder() {
-// 	err := os.RemoveAll(dataFolder)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func eraseDataFolder() {
+	err := os.RemoveAll(dataFolder)
+	if err != nil {
+		panic(err)
+	}
+}
 
-// func seedDatabaseWithTestRecords(d *DB) {
-// 	for i := 0; i < *seedNumRecords; i++ {
-// 		k := []byte(faker.Word() + faker.Word())
-// 		v := []byte(faker.Word() + faker.Word())
-// 		d.Insert(k, v)
-// 	}
-// }
+func seedDatabaseWithTestRecords(d *DB) {
+	for i := 0; i < *seedNumRecords; i++ {
+		k := []byte(faker.Word() + faker.Word())
+		v := []byte(faker.Word() + faker.Word())
+		d.Insert(k, v)
+	}
+}
